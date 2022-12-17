509ff2097cae17f447cd7b0eb8cde752a0d53abb
Ryan Hasbie <ryanhasbie7@gmail.com>
2022-12-17 14:06:08 +0700

    Create method put

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello Ryan')
 })
 
 app.post('/login', (req, res) => {
-  console.log({requestFromOutSide: req})
+  console.log({requestFromOutSide: req.body})
   res.send('Login Successfully!')
+})
+
+app.put('/username', (req, res) => {
+  console.log({updateData: req.body})
+  res.send('Update Success!')
 })
 
 app.listen(port, () => {
